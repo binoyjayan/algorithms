48a6d7f2854fffa3d8fd2a39f7b57bcff9219d16
Binoy Jayan <binoyjayan@gmail.com>
2025-01-13 07:15:12 -0800

    binary trees: add symmetic check

--- binary-trees/invert_binary_tree.cc
+++ binary-trees/invert_binary_tree.cc
@@ -23,11 +23,23 @@ public:
 void invertBinaryTree(BinaryTree *tree) {
     if (tree->left != nullptr) {
         invertBinaryTree(tree->left);
-  }
+    }
     if (tree->right != nullptr) {
         invertBinaryTree(tree->right);
-  }
+    }
     std::swap(tree->left, tree->right);
 }
 
+bool isMirror(TreeNode* left, TreeNode* right) {
+    if (!left || !right)
+        return left == right;
+    return (left->val == right->val) 
+        && isMirror(left->left, right->right)
+        && isMirror(left->right, right->left);
+}
 
+bool isSymmetric(TreeNode* root) {
+    if (!root)
+        return true;
+    return isMirror(root->left, root->right);    
+}
